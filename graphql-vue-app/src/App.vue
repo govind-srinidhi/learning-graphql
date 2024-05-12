8c1b4c1fd48620331a52315d012602ebb6d9b8aa
Govind <25.govind@gmail.com>
2024-05-12 14:47:28 +0200

    (feat) Improve DX in GraphQL client

--- graphql-vue-app/src/App.vue
+++ graphql-vue-app/src/App.vue
@@ -1,47 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import gql from 'graphql-tag';
 import { useQuery } from '@vue/apollo-composable';
-
-const ALL_BOOKS_QUERY = gql`
-    query AllBooks {
-        allBooks {
-            id
-            title
-            description
-        }
-    }
-`;
+import ALL_BOOKS_QUERY from './graphql/allBooks.query.gql';
 
 const { result } = useQuery(ALL_BOOKS_QUERY);
-console.log(result);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-
-  <div v-for="book in result.allBooks">{{ book.title }}</div>
-  <HelloWorld msg="Vite + Vue" />
+  <div v-for="book in result?.allBooks">{{ book.title }}</div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
